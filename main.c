68aa27a724808fd1986ff9f49f7175352dbb8afa
adr-wisniewski <adr.wisniewski@gmail.com>
2017-01-15 19:21:49 -0800

    Update main.c

--- main.c
+++ main.c
@@ -8,11 +8,15 @@ struct lista {
 	struct lista *nastepna;                      //wskaznik na nastepny element
 };
 
+// TODO Zamiast powtarzac struct lista, mozesz uzyc typedef.
+// TODO Poza tym, 0 zmiennych globalnych. Usun ta zmienna.
 struct lista *pierwszy = NULL;                   //wskaznik na poczatek listy
 
+// TODO Staraj sie nie mieszac funkcji listy i interfejsu uzytkownika, zlacz utworzelement i dodaj. Zmien parametry dodaj(lista*, int wartosc).
+// Wyrzuc scanfy i printfy do petli glownej programu.
 struct lista *utworzelement() {                   //tworzy element bez wsp.
 	int wartoscdanej;
-	struct lista*tmp=(struct lista*)malloc(sizeof(struct lista));   //zasada dzia³ania jak "new" w c++
+	struct lista*tmp=(struct lista*)malloc(sizeof(struct lista));   //zasada dziaÅ‚ania jak "new" w c++
 	printf("Podaj dana: "); 
 	scanf("%d", &wartoscdanej); 
 	printf("\n");
@@ -22,6 +26,7 @@ struct lista *utworzelement() {                   //tworzy element bez wsp.
 };
 
 struct lista *dodaj () {        //utworz, potem wsp
+	// TODO Zamiast uzywac zmiennych globalnych, dodaj lista* do parametrow funkcji.
         struct lista *nowa = utworzelement();         //nowy element
 		struct lista *temp = pierwszy;                //pomocnicza do przemieszczania sie po liscie
 		while (temp->nastepna) {                      //przesuniecie na ostatni element
@@ -31,9 +36,10 @@ struct lista *dodaj () {        //utworz, potem wsp
         nowa->nastepna = 0;                           //wskaznik kolejnego na 0 - koniec listy
 };
 
+// TODO: nr sugeruje ze usuwasz element o pozycji nr a nie wartosci. Zmien na dana/wartosc.
 struct lista *usun (int nr) {
     struct lista *t = pierwszy;                      //pomocnicza 1.
-    int licznik = 1;                                 //numeracja elementu
+    int licznik = 1;                                 //numeracja elementu // TODO: zamien licznik na lista* poprzedni
 	while (t) {
 		if (t->dana == nr) {
     		if (licznik == 1) {                      //jesli to pierwszy element
@@ -50,6 +56,8 @@ struct lista *usun (int nr) {
             		temp = temp->nastepna;
         		}
             	temp->nastepna = temp->nastepna->nastepna;
+			// TODO: czy ta funkcja ma usuwac wszystkie elementy o zadanej wartosci? jezeli tak to zmien nazwe na
+			// usun_wszystkie. Poza tym, nie musisz iterowac od poczatku.
 				t = pierwszy;
     			licznik = 1;
     		}
@@ -78,7 +86,8 @@ void wyswietlliste() {
 
 
 int main(int argc, char *argv[]) {
-	
+	// TODO: tutaj mozesz stworzyc instancje listy zamiast uzywac zmiennych globalnych.
+	// TODO: nigdy nie nazywaj zmiennych a b x y, zmien na wybor.
 	int x;
 	do {
 			printf("LISTA:\n");
@@ -87,6 +96,7 @@ int main(int argc, char *argv[]) {
 		
 		printf("MENU:\n1. Dodaj.\n2. Usun.\n3.Wyjdz\n");
 		scanf(" %d", &x);
+		// TODO: dlaczego nie switch(x)
 		if (x==1) {
 			if (pierwszy == NULL) {
 				pierwszy = utworzelement();
@@ -98,8 +108,10 @@ int main(int argc, char *argv[]) {
 			printf("Ktory usunac?");
 			scanf(" %d", &y);
 			usun(y);
+	        // TODO: jak ktos wpisze niepoprawnych wybor to wyjdzie
 		}else break;
 		system("cls");    //czysci ekran
+	// TODO: czyli jak ktos wpisze 4 to wyjdzie i tak. Zmien do/while na while(true)
 	}while (x!=4);
 	return 0;
 }
